f0746ab11411e5aaa21873951ff682ef0e2c5b53
mcollins092397 <mcollins092397@ksu.edu>
2021-12-15 11:30:04 -0600

    finished second example on lesson 1 part 3

--- lesson1p3.js
+++ lesson1p3.js
@@ -1,6 +1,6 @@
-var submitAnswer = function() {
+var submitAnswerL1E1 = function() {
 
-  var radios = document.getElementsByName('choice');
+  var radios = document.getElementsByName('choiceL1E1');
   var val= "";
   for (var i = 0, length = radios.length; i < length; i++) {
       if (radios[i].checked) {
@@ -21,5 +21,31 @@ var submitAnswer = function() {
   }
 	else if (val == "Missing Curly Brackets") {
     alert("That is incorrect, remember that we need to include curly brackets {} after the class declaration to make it's body");
+  }
+};
+
+var submitAnswerL1E2 = function() {
+
+  var radios = document.getElementsByName('choiceL1E2');
+  var val= "";
+  for (var i = 0, length = radios.length; i < length; i++) {
+      if (radios[i].checked) {
+         val = radios[i].value; 
+         break;
+       }
+  }
+  
+  if (val == "" ) {
+    alert('please select choice answer');
+  } else if ( val == "Correct" ) {
+    alert('Answer is correct !');
+  } else if (val == "Missing String") {
+    alert('While this is a correct way to declare a method it is incorrect for the main method. The main method must include (String[] args)');
+  }
+	else if (val == "Incorrect Declaration") {
+    alert('That is incorrect, remember that the main method is declared differently than a class. It is declared using public static void main(String[] args) followed by its body');
+  }
+	else if (val == "Missing Curly Brackets") {
+    alert("That is incorrect, remember that we need to include curly brackets {} after the main declaration to make it's body");
   }
 };
